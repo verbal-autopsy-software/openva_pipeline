7e7764afd0a189d50473d65b56d89530a3dd3e0e
jarathomas <jarathomas@gmail.com>
2018-11-01 14:56:03 -0400

    Docs not showing up.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openva_pipeline",
-    version="0.0.0.9006",
+    version="0.0.0.9007",
     author="Jason Thomas, Samuel J. Clark, & Martin Bratschi",
     author_email="jarthomas@gmail.com",
     description="Automates the processing of verbal autopsy data.",
