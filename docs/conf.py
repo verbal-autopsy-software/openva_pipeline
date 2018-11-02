d2dd1bb33f89997c60752f4355e58c70bc55ae95
jarathomas <jarathomas@gmail.com>
2018-11-02 17:53:04 -0400

    fixed error (need to import sys before MOCK block)

--- docs/conf.py
+++ docs/conf.py
@@ -14,13 +14,13 @@
 #
 
 import mock
+import sys
+import os
  
 MOCK_MODULES = ['pysqlcipher3', 'pysqlcipher3.dbapi2']
 for mod_name in MOCK_MODULES:
 sys.modules[mod_name] = mock.Mock()
 
-import os
-import sys
 sys.path.insert(0, os.path.abspath('..'))
 import openva_pipeline
 
