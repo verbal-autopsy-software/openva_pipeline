1a20afb87ed17de2c9aa4b8267fdb83b414ae0e7
jarathomas <jarathomas@gmail.com>
2018-11-02 17:50:56 -0400

    trying mock modules again (with submodules)

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,12 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
+import mock
+ 
+MOCK_MODULES = ['pysqlcipher3', 'pysqlcipher3.dbapi2']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
