b093cba7e4cb6a63b7c77da2c2c93e0784e90edb
jarathomas <jarathomas@gmail.com>
2018-11-02 17:32:51 -0400

    fixed typo

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.abspath('..'))
+sys.path.insert(0, os.path.abspath('..'))
 import openva_pipeline
 
 # -- Project information -----------------------------------------------------
