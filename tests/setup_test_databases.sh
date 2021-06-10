d96e7cd1b2f7ecc4b17e0b0565a6e994492b057e
jarathomas <thomas.3912@osu.edu>
2021-06-10 02:24:00 -0400

    updating appveyor

--- tests/setup_test_databases.sh
+++ tests/setup_test_databases.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# Pipeline.db
-#rm Pipeline.db
-#sqlcipher -cmd "pragma key = 'enilepiP'" Pipeline.db -init pipelineDB.sql .exit
-
 # copy_Pipeline.db
 rm copy_Pipeline.db 
 sqlcipher -cmd "pragma key = 'enilepiP'" copy_Pipeline.db -init copy_pipelineDB.sql .exit
 
-# run_pipeline.db
+# run_Pipeline.db
 rm run_Pipeline.db
 sqlcipher -cmd "pragma key = 'enilepiP'" run_Pipeline.db -init run_pipelineDB.sql .exit
 
-# wrongFields_pipeline.db
-rm wrongFields_pipeline.db
-sqlcipher -cmd "pragma key = 'enilepiP'" wrongFields_pipeline.db -init wrongFields_pipelineDB.sql .exit
+# wrongFields_Pipeline.db
+rm wrongFields_Pipeline.db
+sqlcipher -cmd "pragma key = 'enilepiP'" wrongFields_Pipeline.db -init wrongFields_pipelineDB.sql .exit
 
-# wrongTables_pipeline.db
-rm wrongTables_pipeline.db
-sqlcipher -cmd "pragma key = 'enilepiP'" wrongTables_pipeline.db -init wrongTables_pipelineDB.sql .exit
+# wrongTables_Pipeline.db
+rm wrongTables_Pipeline.db
+sqlcipher -cmd "pragma key = 'enilepiP'" wrongTables_Pipeline.db -init wrongTables_pipelineDB.sql .exit
